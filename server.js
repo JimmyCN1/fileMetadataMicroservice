cf26a6b609a061e4c21bebf18dde5c7b4023fe38
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-22 23:45:25 +1000

    imp. multer fielupload

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 "use strict";
 
 var express = require("express");
+var bodyParser = require("body-parser");
 var cors = require("cors");
 var mongoose = require("mongoose");
+
+var multer = require("multer");
+var upload = multer({ dest: "./uploads" });
+
 require("dotenv").config();
 
-// require and use "multer"...
-
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
 
@@ -24,8 +31,13 @@ app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.get("/hello", function(req, res) {
-  res.json({ greetings: "Hello, API" });
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file);
+  res.json({
+    fileName: req.file.originalname,
+    fileType: req.file.mimetype,
+    fileSize: `${req.file.size / 1000}KB`
+  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
